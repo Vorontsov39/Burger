f7e0b7d1bda4a65a3ae2fb0064d96feac58cd56f
Vorontsov39 <vorontsov39rus@gmail.com>
2022-10-08 19:45:26 +0200

    add Header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import AppHeader from '../AppHeder/AppHeader';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppHeader />
     </div>
   );
 }
